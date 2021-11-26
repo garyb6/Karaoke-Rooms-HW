38e67e401d25a6d72c8ffb4a549d2ceb738505e0
garyb6 <gary.bennett6@outlook.com>
2021-11-26 16:38:10 +0000

    do lists start empty tested

--- tests/room_test.py
+++ tests/room_test.py
@@ -15,3 +15,14 @@ class TestRoom(unittest.TestCase):
 
     # def test_room_has_entry_fee(self):
     #     self.assertEqual(5, self.room.entry_fee) 
+
+    def test_guest_list_starts_off_empty(self):
+        self.assertEqual([], self.room.guest_list)
+    
+    def test_playlist_starts_off_empty(self):
+        self.assertEqual([], self.room.playlist)
+
+    def test_can_add_customers(self):
+        self.event.add_customer(self.customer)
+        self.event.add_customer(self.customer2)
+        self.assertEqual(2, len(self.event.customers))
